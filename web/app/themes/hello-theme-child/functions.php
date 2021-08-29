ee6f3fb98388c20779cc977909ed267a2825ee1d
Daniel Jonguitud <danieljonguitud17@outlook.com>
2021-08-29 16:29:38 -0700

    checking exceprt length

--- web/app/themes/hello-theme-child/functions.php
+++ web/app/themes/hello-theme-child/functions.php
@@ -24,7 +24,7 @@ add_action( 'wp_enqueue_scripts', 'hello_elementor_child_enqueue_scripts', 20 );
 
 function changeExcerptLength() : int
 {
-    return 23;
+    return 24;
 }
 
 add_filter('excerpt_length', 'changeExcerptLength');
